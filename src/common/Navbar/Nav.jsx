44def58b5ec24bdb8d5101bffc38c35ec57d2546
Fardin7864 <fardinhasan1888@gmail.com>
2024-01-17 16:36:59 +0600

    make the navbar responsive for all device

--- src/common/Navbar/Nav.jsx
+++ src/common/Navbar/Nav.jsx
@@ -1,24 +1,43 @@
 import { useState } from "react";
 import { GoPerson } from "react-icons/go";
-import { IoIosArrowDown, IoIosArrowUp  } from "react-icons/io";
-
+import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
+import { CiSearch } from "react-icons/ci";
 
 const Nav = () => {
-const [isOpen, setOpen] = useState(false);
-
+  const [isOpen, setOpen] = useState(false);
 
   return (
     <>
-      <div className=" grid grid-cols-3">
-        <div className="border flex justify-center">
-          <h1 className=" font-popins text-4xl font-bold logo">pti.</h1>
+      <div className=" grid grid-cols-5 lg:grid-cols-4 bg-transparent mt-3 justify-between border-b-2 md:border-b-0 pb-3">
+        <div className=" flex justify-center items-center">
+          <h1 className=" font-popins text-2xl md:text-4xl font-bold logo">pti.</h1>
         </div>
-        <div className="border flex justify-center">
-          <div></div>
-          <div>
+        <div className=" flex justify-center items-center lg:col-span-2 md:col-span-3 col-span-4 lg:gap-2">
+          <div className=" flex items-center md:w-9/12 bg-white rounded-lg">
+            <CiSearch className="text-2xl text-p relative left-3" />
+            <input
+              type="search"
+              name=""
+              id=""
+              placeholder="Search Audionbook"
+              className=" rounded-lg text-xs md:text-sm py-2 md:py-3 w-full pl-4 outline-none"
+            />
+          </div>
+          {/* Menu section */}
+          <div className=" md:w-3/12 w-1/3">
             <details className="dropdown">
-              <summary onClick={() => setOpen(!isOpen)} className="m-1 btn uppercase bg-white flex justify-between items-center gap-10"><span className=" font-bold">Menu</span> {isOpen ? (<IoIosArrowUp className=" text-2xl text-p"/>) : (<IoIosArrowDown className=" text-2xl text-p"/>)}</summary>
-              <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+              <summary
+                onClick={() => setOpen(!isOpen)}
+                className="m-1 md:btn md:bg-white btn-sm rounded-md uppercase bg-white flex md:flex-row justify-between items-center"
+              >
+                <span className=" font-bold text-xs md:text-sm">Menu</span>{" "}
+                {isOpen ? (
+                  <IoIosArrowUp className=" lg:text-xl md:text-lg text-p" />
+                ) : (
+                  <IoIosArrowDown className=" lg:text-xl md:text-lg text-p" />
+                )}
+              </summary>
+              <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box">
                 <li>
                   <a>Item 1</a>
                 </li>
@@ -29,8 +48,8 @@ const [isOpen, setOpen] = useState(false);
             </details>
           </div>
         </div>
-        <div className="border flex justify-center">
-          <button className=" p-4 rounded-full bg-[#fd6011]">
+        <div className=" flex justify-center">
+          <button className=" hidden md:block p-4 rounded-full bg-[#fd6011]">
             <GoPerson className=" text-2xl text-white bg-[#fd6011]" />
           </button>
         </div>
